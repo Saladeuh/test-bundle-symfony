fd2ccc1d9ab6b059d4d1d76d0dd834f41edac4bc
Saladeuh <emilienmonet10@gmail.com>
2024-02-16 10:54:57 +0100

    change controller namespace

--- src/Controller/TestBundleController.php
+++ src/Controller/TestBundleController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace TestBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
