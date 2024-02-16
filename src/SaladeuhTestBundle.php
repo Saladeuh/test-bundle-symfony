d98acdf8311c1f3406063f0d5cb91fcf3f89359a
Saladeuh <emilienmonet10@gmail.com>
2024-02-16 13:22:59 +0100

    add config service loader

--- src/SaladeuhTestBundle.php
+++ src/SaladeuhTestBundle.php
@@ -1,10 +1,15 @@
 <?php
 namespace TestBundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SaladeuhTestBundle extends AbstractBundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yyaml');
+    }
     public function getPath(): string
     {
         return \dirname(__DIR__);
