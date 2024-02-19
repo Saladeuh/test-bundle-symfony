f43810013f2420ea6be9753609d6f5c08ff14aa2
Saladeuh <emilienmonet10@gmail.com>
2024-02-19 10:08:37 +0100

    rename bundle class to short name

--- src/TestBundle.php
+++ src/TestBundle.php
@@ -4,7 +4,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class SaladeuhTestBundle extends AbstractBundle
+class TestBundle extends AbstractBundle
 {
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
