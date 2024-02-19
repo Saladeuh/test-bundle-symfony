3f9f40057e41bdab26b99ee77e9ee706ce3f2dad
Saladeuh <emilienmonet10@gmail.com>
2024-02-19 10:15:16 +0100

    change extractbundle to bundle

--- src/TestBundle.php
+++ src/TestBundle.php
@@ -3,8 +3,9 @@ namespace TestBundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class TestBundle extends AbstractBundle
+class TestBundle extends Bundle
 {
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
